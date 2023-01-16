73ae68430ce801a587cd3b4ba8b020127841b651
alisradwan <aloshradwan44@gmail.com>
2023-01-16 15:17:00 -0800

    fixed path

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // import custom components
-import Header from './components/Header';
-import About from './pages/About';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
-import Terminal from './pages/Terminal';
-import Footer from './components/Footer';
-import ScrollToTop from './components/ScrollToTop';
-import NoMatch from './pages/NoMatch';
+import Header from "./components/Header";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+import Terminal from "./pages/Terminal";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import NoMatch from "./pages/NoMatch";
 
 // import styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-typist/dist/Typist.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-typist/dist/Typist.css";
+import "./App.css";
 
-function App(){
+function App() {
   return (
     <Router>
       <ScrollToTop />
       <div className="App">
-        <Header/>
+        <Header />
         <main>
           <Switch>
-            <Route exact path="/" component={Terminal} />
+            <Route exact path="/Portfolio" component={Terminal} />
             <Route exact path="/about" component={About} />
             <Route exact path="/projects" component={Projects} />
             <Route exact path="/resume" component={Resume} />
@@ -33,7 +33,7 @@ function App(){
             <Route path="/*" component={NoMatch} />
           </Switch>
         </main>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
